f69a64ae46dfcdacd7fae3be5b6f4d0cc181c79b
Vinit Soni <vinitsoni9869@gmail.com>
2018-03-24 14:45:17 -0400

    add basic error handler for Unhandled Promise Rejections

--- app.js
+++ app.js
@@ -28,9 +28,9 @@ app.use('/questions', questions);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handler
@@ -39,9 +39,9 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // render the error
   res.status(err.status || 500);
-  res.render('error');
+  res.end(err.toString());
 });
 
 module.exports = app;
--- config/basic.js
+++ config/basic.js
@@ -1,10 +1,14 @@
 var errHandler = function (err, obj) {
 	if (err) { throw err; }
 }
+
+var errWrap = (fn) => (...args) => fn(...args).catch(args[2]);
+
 var routeCheck = function(res, endpoint) {
 	res.end("Reached: " + endpoint);
 };
 
 module.exports.errHandler = errHandler;
+module.exports.errWrap = errWrap;
 module.exports.routeCheck = routeCheck;
 
