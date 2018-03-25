4c2a73c647e635ed431866935331896c7a6da44b
Vinit Soni <vinitsoni9869@gmail.com>
2018-03-25 16:10:20 -0400

    add redis auth

--- config/externals.js
+++ config/externals.js
@@ -5,5 +5,13 @@ mongoose.connect(dbUrl);
 
 const redis = Promise.promisifyAll(require('redis').createClient(process.env.PICKY_REDIS_SESSIONS_PORT, process.env.PICKY_REDIS_SESSIONS_HOST));
 
+var redis = require('redis').createClient(process.env.PICKY_REDIS_SESSIONS_PORT, process.env.PICKY_REDIS_SESSIONS_HOST)
+
+if (process.env.NODE_ENV !== "development") {
+	redis.auth(process.env.REDIS_SESSION_AUTH_KEY, (err) => {
+		if (err) throw err;
+	})
+}
+
 module.exports.mongoose = mongoose;
 module.exports.redis = redis;
