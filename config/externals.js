09bea83857caa18a68e4832805e9ddf963d87adf
Vinit Soni <vinitsoni9869@gmail.com>
2018-03-25 16:12:27 -0400

    fix double declare error

--- config/externals.js
+++ config/externals.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const dbUrl = process.env.PICKY_DB_URL;
 mongoose.connect(dbUrl);
 
-const redis = Promise.promisifyAll(require('redis').createClient(process.env.PICKY_REDIS_SESSIONS_PORT, process.env.PICKY_REDIS_SESSIONS_HOST));
+// const redis = Promise.promisifyAll(require('redis').createClient(process.env.PICKY_REDIS_SESSIONS_PORT, process.env.PICKY_REDIS_SESSIONS_HOST));
 
 var redis = require('redis').createClient(process.env.PICKY_REDIS_SESSIONS_PORT, process.env.PICKY_REDIS_SESSIONS_HOST)
 
