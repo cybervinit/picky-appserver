237f6753db3f9fce0a2ee8342c57b95c61884f3f
Vinit Soni <vinitsoni9869@gmail.com>
2018-04-14 13:57:32 -0400

    change question structure in mongo

--- routes/questions.js
+++ routes/questions.js
@@ -1,15 +1,46 @@
 var express = require('express');
 var router = express.Router();
 
-var mongoose = require('../config/externals.js').mongoose;
-var models = require('../models');
+const { Question, QuestionOption } = require('../models');
+var ObjectId = require('mongodb').ObjectID;
+const { errWrap, errHandler } = require('../config/basic.js');
 var basic = require('../config/basic.js');
 
 
 router.get('/', (req, res, next) => {
 	const { routeCheck } = basic;
-	const { QuestionBucket } = models;
 	routeCheck(res, "questions");
 });
+
+/**
+ * Body: question: { username: String, options: [], title: String, visibility: Number }
+ */
+router.post('/createQuestion', errWrap( async (req, res, next) => {
+	var optList = [];
+	for (opt in req.body.question.options) { 
+		optList.push({ title: req.body.question.options[opt], pickers: [], index: Number(opt)});
+	}
+	const newQuestion = { username: req.body.username, title: req.body.question.title, options: optList, visibility: req.body.question.visibility };
+	await Question.create(newQuestion);
+	res.end("successful.");
+}));
+
+/**
+ * Body: picker: String, optionId: String
+ */
+router.post('/castVote', errWrap(async (req, res, next) => {
+	const asker = req.body.qAskerUsername;
+	const qId = new ObjectId(req.body.qId);
+	const option = req.body.optionIndex;
+	// TODO: assert correct value
+	// TODO: add session check 
+	// TODO: cast the vote
+	await QuestionOption.create({
+		picker: req.body.picker,
+		optionId: ObjectId(req.body.optionId)
+	});
+	res.end('successful.');
+}));
+
 
 module.exports = router;
