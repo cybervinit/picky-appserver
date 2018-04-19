047043fad552d4a4b78e16ed4d65533f77637b06
Vinit Soni <vinitsoni9869@gmail.com>
2018-04-18 22:53:23 -0400

    add /login route

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var router = express.Router()
+const bcrypt = require('bcrypt')
 // var mongoose = require('../config/externals.js').mongoose;
 const { User } = require('../models')
 const { errWrap, errHandler } = require('../config/basic.js')
@@ -19,21 +20,52 @@ router.get('/', errWrap(async (req, res, next) => {
  *
  * @apiParam {String} username The username of the new user
  * @apiParam {String} phone The phone number of the new user
+ * @apiParam {String} passwordHash The passwordHash of the new user
  */
 router.post('/registerUser', errWrap(async (req, res, next) => {
-  assert.notStrictEqual(req.body.username, undefined, 'username not provided')
-  assert.notStrictEqual(req.body.phone, undefined, 'phone not provided')
-  const preexistent = await User.findOne({ username: req.body.username })
+  const { username, phone, passwordHash } = req.body
+  assert.notStrictEqual(username, undefined, 'username not provided')
+  assert.notStrictEqual(phone, undefined, 'phone not provided')
+  const preexistent = await User.findOne({ username: username })
   assert.strictEqual(preexistent, null, 'username already exists')
-  await User.create({ username: req.body.username, phone: req.body.phone }, errHandler)
+  const finalPasswordHash = await bcrypt.hash(passwordHash, 4) // Salt rounds 4
+  console.log(finalPasswordHash)
+  await User.create({ username: username, phone: phone, passwordHash: finalPasswordHash }, errHandler)
   res.end('successful')
 }))
 
-router.get('/setsession', errWrap(async (req, res, next) => {
-  await a.setSessionID('vinso', 'yayaya')
-  res.end('authenticated.')
+/**
+ * @api {post} /users/login Login a user
+ * @apiName Login
+ * @apiGroup User
+ *
+ * @apiParam {String} username The username of the login attempting user
+ * @apiParam {String} passwordHash The hashed password of the user
+ *
+ */
+router.post('/login', errWrap(async (req, res, next) => {
+  const { username, passwordHash } = req.body
+  const user = await User.findOne({ username: username })
+  if (user) {
+    const sessId = await a.setSessionID(username) // Also returns the session ID
+    bcrypt.compare(passwordHash, user.passwordHash, (err, result) => {
+      if (err) return res.end(JSON.stringify({ loginAttemptResult: 'fail' }))
+      if (result === true) {
+        return res.end(JSON.stringify({
+          loginAttemptResult: 'success',
+          sessionID: sessId
+        }))
+      }
+      return res.end(JSON.stringify({
+        loginAttemptResult: 'fail'
+      }))
+    })
+  }
 }))
 
+/**
+ * @api {get} /getByUsername/:username Get user information
+ */
 router.get('/getByUsername/:username', errWrap(async (req, res, next) => {
   var username = req.params.username
   var user = await User.findOne({ 'username': username })
