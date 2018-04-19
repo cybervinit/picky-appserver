c4cad23a54787a3b7061c0933040d15a32c5dbc1
Vinit Soni <vinitsoni9869@gmail.com>
2018-04-18 22:53:23 -0400

    add /login route

--- models/user.js
+++ models/user.js
@@ -1,14 +1,13 @@
-const mongoose = require('../config/externals.js').mongoose;
-const Schema = mongoose.Schema;
-
+const mongoose = require('../config/externals.js').mongoose
+const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
-	username: { type: String, lowercase: true },
-	phone: String,
-	followRequests: [String],
-	followers: [String],
-	following: [String],
-});
+  username: { type: String, lowercase: true },
+  phone: String,
+  passwordHash: String,
+  followRequests: [String],
+  followers: [String],
+  following: [String]
+})
 
-module.exports.User = mongoose.model('User', UserSchema);
-
+module.exports.User = mongoose.model('User', UserSchema)
