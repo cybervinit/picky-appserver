d960dfb6198b1717cc3ed42248d1d6db92993f7c
Vinit Soni <vinitsoni9869@gmail.com>
2018-10-22 17:42:38 -0400

    add mobile signin flow

--- src/models/user.js
+++ src/models/user.js
@@ -4,12 +4,11 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   username: { type: String, lowercase: true },
   name: { type: String, lowercase: true },
-  phone: String,
-  passwordHash: String,
-  followRequests: [String],
-  followers: [String],
-  following: [String],
-  profileVisible: { type: Boolean, default: true }
+  friendAmount: { type: Number, default: 0 },
+  friendList: [String],
+  profileVisible: { type: Boolean, default: true },
+  googleId: String,
+  isNewAccount: Boolean
 });
 
 module.exports.User = mongoose.model('User', UserSchema);
