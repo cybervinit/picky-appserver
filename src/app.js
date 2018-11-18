246ae28f9d1736572fbff118ec40452e2807195e
Vinit Soni <cybervinit@users.noreply.github.com>
2018-11-18 16:53:57 -0500

    connected game-session routes to main app

--- src/app.js
+++ src/app.js
@@ -41,6 +41,7 @@ app.get('/', (req, res) => {
 auth(app);
 require('./routes/friends')(app);
 app.use('/users', users);
+require('./routes/game-sessions')(app);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- src/routes/game-sessions.js
+++ src/routes/game-sessions.js
@@ -3,17 +3,18 @@ const router = express.Router();
 const { errWrap } = require('../config/basic');
 const { getGameSession } = require('../models');
 
+module.exports = app => {
+  app.get('/:sessionName', errWrap(async (req, res, next) => {
+    const { sessionName } = req.params;
+    const gameSession = await getGameSession(sessionName);
+    console.log("Game Session: ", gameSession);
+    res.send({
+      gameSessionExists: gameSession ? true : false,
+      gameSession: {
+        name: gameSession.name
+      }
+    });
+    res.end();
+  }));
+}
 
-
-router.get('/:sessionName', errWrap(async (req, res, next) => {
-  const { sessionName } = req.params;
-  const gameSession = await getGameSession(sessionName);
-  console.log("Game Session: ", gameSession);
-  res.send({
-    gameSessionExists: gameSession ? true : false,
-    gameSession: {
-      name: gameSession.name
-    }
-  });
-  res.end();
-}));
