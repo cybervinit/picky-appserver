dc4235533df8189bb098d21aef4099af0042765c
Vinit Soni <vinitsoni9869@gmail.com>
2019-05-14 16:04:47 -0400

    update fe url

--- src/app.js
+++ src/app.js
@@ -44,12 +44,12 @@ app.use(cookieSession({
 app.use((req, res, next) => {
   /* req.app.get('env') === 'development' */
   if (req.app.get('env') === 'production') {
-    res.setHeader('Access-Control-Allow-Origin', 'https://piky.me');
+    res.setHeader('Access-Control-Allow-Origin', 'https://www.piky.me');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Add headers (sent from CORS request) here
     // TODO: switch to use the cors npm package
-    if (req.headers.origin !== 'https://piky.me') return;
+    if (req.headers.origin !== 'https://piky.me') return; // TODO: throw 403 Forbidden
   } else if (req.app.get('env') === 'development') {
     res.setHeader('Access-Control-Allow-Origin', "http://localhost:4200");
   }
